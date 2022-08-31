e03ab83c21323f7a38742c5b3e850a4f684b1e2b
Manu Devos <manudevos@gmail.com>
2022-08-31 13:46:00 +0200

    Placed variable inside function

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-let ComputerOptions = ["Rock", "Paper", "Scissors"]
-
 function getComputerChoice() {
+    let ComputerOptions = ["Rock", "Paper", "Scissors"]
     let ComputerChoice = ComputerOptions[Math.floor(Math.random()*ComputerOptions.length)]
     console.log(ComputerChoice)
 }
