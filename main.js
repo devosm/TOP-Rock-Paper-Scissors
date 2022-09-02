731cd145649238062cddf09fdca73e72f72becce
Manu Devos <manudevos@gmail.com>
2022-09-02 12:20:08 +0200

    Added round win/lose conditions

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
 function getComputerChoice() {
-    let ComputerOptions = ["Rock", "Paper", "Scissors"]
+    let ComputerOptions = ["ROCK", "PAPER", "SCISSORS"]
     const ComputerChoice = ComputerOptions[Math.floor(Math.random()*ComputerOptions.length)]
     return ComputerChoice
 }
 
 function getPlayerChoice() {
-    const PlayerChoice = "rock";
+    const PlayerChoice = "scissors";
     return PlayerChoice
 }
 
-const PlayerSelection = getPlayerChoice()
+const PlayerSelection = getPlayerChoice().toUpperCase()
 const ComputerSelection = getComputerChoice()
 
 function playRound() {
-    console.log("Computer chooses: " + ComputerSelection + " and Player chooses: " + PlayerSelection)
+    console.log("Player Selected: " + PlayerSelection)
+    console.log("Computer Selected: " + ComputerSelection)
+    if (PlayerSelection == ComputerSelection) {
+        console.log("Draw, you both selected " + PlayerSelection)
+    } else if (PlayerSelection == "PAPER" && ComputerSelection == "SCISSORS") {
+        console.log("You lose, SCISSORS beats PAPER")
+    } else if (PlayerSelection == "PAPER" && ComputerSelection == "ROCK") {
+        console.log("You Win! PAPER beats ROCK")
+    } else if (PlayerSelection == "SCISSORS" && ComputerSelection == "ROCK") {
+        console.log("You lose, ROCK beats SCISSORS")
+    } else if (PlayerSelection == "SCISSORS" && ComputerSelection == "PAPER") {
+        console.log("You Win! SCISSORS beats PAPER")
+    } else if (PlayerSelection == "ROCK" && ComputerSelection == "PAPER") {
+        console.log("You lose, SCISSORS beats PAPER")
+    } else if (PlayerSelection == "ROCK" && ComputerSelection == "SCISSORS") {
+        console.log("You Win! ROCK beats SCISSORS")
+    }
 }
 
 playRound()
