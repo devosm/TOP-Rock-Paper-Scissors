ed087eabc26ac66c8e365f5c8f22926121bed65c
Manu Devos <manudevos@gmail.com>
2022-09-06 23:51:18 +0200

    Added scoring system

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let computerScore = 0
+
 function getComputerChoice() {
     let ComputerOptions = ["ROCK", "PAPER", "SCISSORS"]
     const ComputerChoice = ComputerOptions[Math.floor(Math.random()*ComputerOptions.length)]
@@ -19,24 +22,43 @@ function playRound() {
         playRound()
     } else if (PlayerSelection == ComputerSelection) {
         console.log("Draw, you both selected " + PlayerSelection)
+        playerScore = playerScore + 1
+        computerScore = computerScore + 1
     } else if (PlayerSelection == "PAPER" && ComputerSelection == "SCISSORS") {
         console.log("You lose, SCISSORS beats PAPER")
+        computerScore = computerScore + 1
     } else if (PlayerSelection == "PAPER" && ComputerSelection == "ROCK") {
         console.log("You Win! PAPER beats ROCK")
+        playerScore = playerScore + 1
     } else if (PlayerSelection == "SCISSORS" && ComputerSelection == "ROCK") {
         console.log("You lose, ROCK beats SCISSORS")
+        computerScore = computerScore + 1
     } else if (PlayerSelection == "SCISSORS" && ComputerSelection == "PAPER") {
         console.log("You Win! SCISSORS beats PAPER")
+        playerScore = playerScore + 1
     } else if (PlayerSelection == "ROCK" && ComputerSelection == "PAPER") {
         console.log("You lose, SCISSORS beats PAPER")
+        computerScore = computerScore + 1
     } else if (PlayerSelection == "ROCK" && ComputerSelection == "SCISSORS") {
         console.log("You Win! ROCK beats SCISSORS")
+        playerScore = playerScore + 1
     }
 }
 
 function game () {
+    let round = 0
     for (let i = 0; i < 5; i++) {
+        round = round + 1
+        console.log("Round: (" + round + "/5)" )
         playRound()
+        console.log("PLAYER " + playerScore + " - " + computerScore + " COMPUTER")
+    }
+    if (playerScore > computerScore) {
+        console.log("Player Wins!")
+    } else if (playerScore == computerScore) {
+        console.log("Draw!")
+    } else {
+        console.log("Computer Wins!")
     }
 }
 
